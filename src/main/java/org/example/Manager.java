e4af514d754a025f492d088241c5a3ab3aea68ca
rdogan7106 <78352219+rdogan7106@users.noreply.github.com>
2022-08-29 23:19:13 +0200

    Update Manager.java

--- src/main/java/org/example/Manager.java
+++ src/main/java/org/example/Manager.java
@@ -4,7 +4,7 @@ public class Manager extends Employee implements IManager{
     private int numberOfEmployeesSupervised;
     @Override
     public void increaseSalary(SoftwareDeveloper softwareDeveloper,int salary) {
-        System.out.println(softwareDeveloper +" increase ");
+        System.out.println(softwareDeveloper.getFirstName() +" increase " +salary);
     }
 
     @Override
@@ -14,16 +14,19 @@ public class Manager extends Employee implements IManager{
 
     } @Override
     public void hireEmployee(Employee employee) {
-        System.out.println(employee.getFirstName() + "hire ");
+        System.out.println(employee.getFirstName() + "hired ");
     }
 
     @Override
     public void fireEmployee(Employee employee) {
 
+        System.out.println(employee.getFirstName() + "fired");
+
     }
 
     @Override
     public void getNumberOfEmployeesSupervised() {
+        System.out.println(" has supervised " + this.numberOfEmployeesSupervised);
 
     }
 
